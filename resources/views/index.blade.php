8c55a42bf71944e77390cf29ce29c307468780ba
nicolefeliceee <nicolefeliceee@gmail.com>
2023-04-09 18:51:44 +0700

    merge

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,19 +32,6 @@
                 </div>
             </div>
 
-            {{-- <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false">
-                <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="800">
-                        <img src="assets/img/banner.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item" data-bs-interval="800">
-                        <img src="assets/img/banner.png" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item" data-bs-interval="800">
-                        <img src="assets/img/banner.png" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-            </div> --}}
 
         </div>
     </div>
