bd3aa9bf4b88cf70fb1a09f969f0c95f961308a2
DenioPranatha <denioindonesia@gmail.com>
2023-06-15 16:24:30 +0700

    Fixing user and event attribute

--- database/migrations/2023_05_03_091218_create_users_table.php
+++ database/migrations/2023_05_03_091218_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 55);
             $table->string('username', 55);
             $table->string('email');
             $table->string('phone', 20);
