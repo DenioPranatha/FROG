fdfb2fe02271056779f701e750e1ecb918b2f1ae
nicolefeliceee <nicolefeliceee@gmail.com>
2023-06-18 18:14:53 +0700

    destination

--- resources/views/destination.blade.php
+++ resources/views/destination.blade.php
@@ -7,6 +7,20 @@
 @section('title', 'Destination')
 
 @section('content')
-    <h1>ini destination</h1>
+    <div class="destination">
+        <div class="atas">
+            <div class="searchDiv">
+                <form action="" method="post" class="h-100 d-flex justify-content-start align-items-center">
+                    <input type="text" name="searchBox" id="searchBox" class="searchBox" placeholder="Search destination for your event">
+                    <div class="searchLogo">
+                        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M26.25 26.25L19.7538 19.7538M19.7538 19.7538C21.512 17.9955 22.4997 15.6109 22.4997 13.1244C22.4997 10.6379 21.512 8.25322 19.7538 6.495C17.9955 4.73678 15.6109 3.74902 13.1244 3.74902C10.6379 3.74902 8.25322 4.73678 6.495 6.495C4.73678 8.25322 3.74902 10.6379 3.74902 13.1244C3.74902 15.6109 4.73678 17.9955 6.495 19.7538C8.25322 21.512 10.6379 22.4997 13.1244 22.4997C15.6109 22.4997 17.9955 21.512 19.7538 19.7538Z" stroke="#522E93" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+    </div>
 @endsection
 
