fa968acc1e7638a275e3c765e722222c6260c925
Ardo Damarjati <damarjatiardo@gmail.com>
2023-06-27 12:13:56 +0700

    Revisi Page

--- resources/views/approval.blade.php
+++ resources/views/approval.blade.php
@@ -7,14 +7,16 @@
 @section('title', 'Approval')
 
 @section('content')
-<div class="container d-flex flex-column align-items-center">
-    <h3>Waiting for Approval</h3>
-    <div class="card-container d-flex flex-row justify-content-left flex-wrap">
-        @foreach ($events as $event)
-            <div class="card-test" data-aos="fade-up">
-                @include('partials.eventCart', ['event' => $event])
-            </div>
-        @endforeach
+<div class="left-right">
+    <div class="container d-flex flex-column align-items-center justify-content-center">
+        <h3>Waiting for Approval</h3>
+        <div class="card-container d-flex flex-row justify-content-left flex-wrap">
+            @foreach ($events as $event)
+                <div class="card-test" data-aos="fade-up">
+                    @include('partials.eventCart', ['event' => $event])
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
