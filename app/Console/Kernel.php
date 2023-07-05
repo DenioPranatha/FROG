ab0c030e9f0a8b951ffaad523b857ee835b156d9
nicolefeliceee <nicolefeliceee@gmail.com>
2023-07-05 15:26:02 +0700

    cobacoba kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,4 +35,8 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    // protected $commands = [
+    //     'App\Console\Commands\Schedule',
+    // ];
 }
